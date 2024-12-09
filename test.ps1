2fca90da58c0eb11077dff7828115f3b42318773
Mark Garner <mgarner@microsoft.com>
2024-12-09 15:54:59 -0600

    remove a test

--- test.ps1
+++ test.ps1
@@ -3,7 +3,7 @@ Import-Module '.\FabricPS' -Force
 
 $capacityId = '11E25ED5-7B83-4C7A-9ECD-EBF8F667CA20'
 $principalId = '60bf0b41-0484-466b-8b46-ebb6692bd8cc'
-$workspaceName = 'Auotmaed_Test_Workspace'
+$workspaceName = 'Automated_Test_Workspace'
 
 Write-Host 'Get-Workspaces'
 $workspaces = Get-Workspaces
@@ -15,7 +15,6 @@ if ($existingWorkspace) {
 }
 Write-Host 'New-Workspace'
 $workspace = New-Workspace -DisplayName $workspaceName -Description 'A test workspace' -CapacityId $capacityId
-$workspace
 Write-Host 'New-WorkspaceRoleAssignment'
 New-WorkspaceRoleAssignment -WorkspaceId $workspace.id -PrincipalId $principalId -PrincipalType 'User' -WorkspaceRole 'Admin'
 Write-Host 'Get-WorkspaceRoleAssignments'
@@ -51,5 +50,5 @@ Get-FabricItems -WorkspaceId $workspace.id -Type 'Notebook'
 Write-Host 'New-FabricItem'
 New-FabricItem -WorkspaceId $workspace.id -Type 'Notebook' -DisplayName 'Created_by_Create_Item'
 Write-Host 'Remove-FabricItem'
-Remove-FabricItem -WorkspaceId $workspace.id -ItemId $anotherNotebook.id
+# Remove-FabricItem -WorkspaceId $workspace.id -ItemId $anotherNotebook.id
 # Update-FabricItem -WorkspaceId $workspace.id -ItemId $anotherNotebook.id -DisplayName 'Updated by Update Item'
