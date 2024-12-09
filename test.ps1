12b5801e8f9d30442d7bb16e596a65de92be4ea4
Mark Garner <mgarner@microsoft.com>
2024-12-09 16:11:55 -0600

    remove tests

--- test.ps1
+++ test.ps1
@@ -30,17 +30,17 @@ try{
     Write-Host 'Remove-Lakehouse'
     Remove-Lakehouse -WorkspaceId $workspace.id -LakehouseId $lakehouse.id
     
-    Write-Host 'New-Notebook'
-    $notebook = New-Notebook -WorkspaceId $workspace.id -DisplayName 'Test_Notebook' -Description 'A test notebook'
-    Write-Host 'New-Notebook'
-    $anotherNotebook = New-Notebook -WorkspaceId $workspace.id -DisplayName 'Test_Notebook_2'
-    Write-Host 'Get-NotebookDefinition'
-    Get-NotebookDefinition -WorkspaceId $workspace.id -NotebookId $notebook.id
-    Write-Host 'Get-Notebooks'
-    Get-Notebooks -WorkspaceId $workspace.id
-    # Update-NotebookDefinition -WorkspaceId $workspace.id -NotebookId $notebook.id -DisplayName 'Updated Notebook'
-    Write-Host 'Remove-Notebook'
-    Remove-Notebook -WorkspaceId $workspace.id -NotebookId $notebook.id
+    # Write-Host 'New-Notebook'
+    # $notebook = New-Notebook -WorkspaceId $workspace.id -DisplayName 'Test_Notebook' -Description 'A test notebook'
+    # Write-Host 'New-Notebook'
+    # $anotherNotebook = New-Notebook -WorkspaceId $workspace.id -DisplayName 'Test_Notebook_2'
+    # Write-Host 'Get-NotebookDefinition'
+    # Get-NotebookDefinition -WorkspaceId $workspace.id -NotebookId $notebook.id
+    # Write-Host 'Get-Notebooks'
+    # Get-Notebooks -WorkspaceId $workspace.id
+    # # Update-NotebookDefinition -WorkspaceId $workspace.id -NotebookId $notebook.id -DisplayName 'Updated Notebook'
+    # Write-Host 'Remove-Notebook'
+    # Remove-Notebook -WorkspaceId $workspace.id -NotebookId $notebook.id
     
     # Write-Host 'Get-FabricItems'
     # Get-FabricItems -WorkspaceId $workspace.id -Type 'Notebook'
